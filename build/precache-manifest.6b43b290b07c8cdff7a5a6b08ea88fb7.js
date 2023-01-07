b25042ad58d05d4d6681c61d8a5750a12d3364f2
Priyanshu Rathore <priyanshurathore0123@gmail.com>
2023-01-07 16:14:27 +0530

    commit

---
+++ build/precache-manifest.6b43b290b07c8cdff7a5a6b08ea88fb7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3670ea494d29c0e48e9a6d5caa600eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e088c0ac5d8e733b190",
+    "url": "/static/js/2.56441421.chunk.js"
+  },
+  {
+    "revision": "807eac502c7503058efd",
+    "url": "/static/js/main.482578db.chunk.js"
+  },
+  {
+    "revision": "2b39c98d96c9238cdd2e",
+    "url": "/static/js/runtime-main.28f67ebe.js"
+  }
+]);
